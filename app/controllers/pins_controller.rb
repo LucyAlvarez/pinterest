3b2731b52e328f0a894efc963b4e59a832dbd5bd
Lucy Alvarez <lucyat94@gmail.com>
2017-06-15 22:19:27 -0500

    users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except:[ :index ]
+  after_action :authenticate_user!, except:[ :show, :index ]
 
   # GET /pins
   # GET /pins.json
